f28cabcbed80936308a7af6270d7d2c382146e8e
vishangi <vishangid@gmail.com>
2022-06-03 16:09:15 +0530

    Added: check two lines are equal or not

---
+++ Uc_2.java
@@ -0,0 +1,38 @@
+package lineComarision;
+
+import java.util.Scanner;
+
+public class Uc_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to line computation program");
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter the x1 value");
+		double x1 = scanner.nextDouble();
+
+		System.out.println("Enter the x2 value");
+		double x2 = scanner.nextDouble();
+
+		System.out.println("Enter the y1 value");
+		double y1 = scanner.nextDouble();
+
+		System.out.println("Enter the y2 value");
+		double y2 = scanner.nextDouble();
+
+		double line1 = Math.sqrt((x2 - x1) + (y2 - y1));
+		System.out.println("First line value is  " + line1);
+		double line2 = Math.sqrt((x2 - x1) - (y2 - y1));
+		System.out.println("Second line value is  " + line2);
+
+		System.out.println("Check equality of the two lines :");
+
+		if (line1 == line2) {
+			System.out.println("Two lines are Equal");
+		} else {
+			System.out.println("Two lines are not Equal");
+		}
+	}
+
+}
